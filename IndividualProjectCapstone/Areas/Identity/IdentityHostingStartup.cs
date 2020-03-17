0ae0f86ba380d8e7770908b59d0db1b3206166b0
Abraham Sanchez <sanchez95san@gmail.com>
2020-03-17 07:50:05 -0500

    innit

---
+++ IndividualProjectCapstone/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IndividualProjectCapstone.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IndividualProjectCapstone.Areas.Identity.IdentityHostingStartup))]
+namespace IndividualProjectCapstone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
